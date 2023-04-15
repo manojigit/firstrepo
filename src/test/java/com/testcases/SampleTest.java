4bf130b72a4cb3f25d60884413fe899807a234f3
manojigit <130819027+manojigit@users.noreply.github.com>
2023-04-15 11:03:47 +0530

    first commit

---
+++ src/test/java/com/testcases/SampleTest.java
@@ -0,0 +1,14 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	
+	
+	
+	@Test
+	public void testCase() {
+		System.out.println("Printed API Testing ::::: ");
+	}
+}
